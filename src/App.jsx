007d39bf9ea1c8cbe3a0aa8ba5c69430bda3976f
Nayeem Uddin <unayeem33@gmail.com>
2024-10-24 10:03:56 +0600

    func moved

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import AllProducts from "./Components/AllProducts";
 import Navbar from "./Components/Navbar";
 import SideBar from "./Components/SideBar";
 
 function App() {
+  // Loading all Products and setting it on var products.
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const res = await fetch("/Data/fakeProduct.json");
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.log("ERROR: ", error);
+      }
+    };
+    loadProducts();
+  }, []);
+
+  // Search Filter
+  const SearchProductByInput = (e) => {
+    console.log(e);
+  };
+
+  // ! Cart Function
+
+  // Cart Items and setting it on var cartItems.
   const [cartItems, setCartItems] = useState([]);
 
+  // Add To cart
   const HAddToCart = (product) => {
     setCartItems([...cartItems, product]);
     // setCartItems((pre) => [...pre, product]);
   };
 
+  // Delete from Cart
   const hDeleteCartItem = (product) => {
     const newArray = cartItems.filter((item) => item.id !== product.id);
 
     setCartItems(newArray);
   };
 
-  const SearchProductByInput = (e) => {
-    console.log(e);
-  };
-
+  // Clear Cart
   const clearCart = () => {
     const noItem = [];
     setCartItems(noItem);
   };
+
   return (
     <>
       <Navbar
@@ -33,7 +56,7 @@ function App() {
         SearchProductByInput={SearchProductByInput}
       ></Navbar>
       <div className="container mx-auto flex px-5 gap-5">
-        <AllProducts HAddToCart={HAddToCart}></AllProducts>
+        <AllProducts products={products} HAddToCart={HAddToCart}></AllProducts>
         <SideBar
           cartItems={cartItems}
           hDeleteCartItem={hDeleteCartItem}
--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,24 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import Product from "./Product";
 import PropTypes from "prop-types";
 
-const AllProducts = ({ HAddToCart }) => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        const res = await fetch("/Data/fakeProduct.json");
-        const data = await res.json();
-        setProducts(data);
-      } catch (error) {
-        console.log("ERROR: ", error);
-      }
-    };
-    loadProducts();
-  }, []);
-
+const AllProducts = ({ HAddToCart, products }) => {
   return (
     <div className="w-2/3">
       <h1 className="text-center font-bold text-2xl py-10">
@@ -39,6 +22,7 @@ const AllProducts = ({ HAddToCart }) => {
 
 AllProducts.propTypes = {
   HAddToCart: PropTypes.func,
+  products: PropTypes.array.isRequired,
 };
 
 export default AllProducts;
